f9e73e0af80b0feb82115461cc7f85675ad5e17c
Dejiang Yin <dj.yin@foxmail.com>
2025-12-30 15:53:33 +0800

    Update ACCEL_sift_drifting.bash

--- python/search_split/ACCEL_sift_drifting.bash
+++ python/search_split/ACCEL_sift_drifting.bash
@@ -5,11 +5,11 @@
 #-------------------------------
 ##
 # Path to Python scripts
-ACCEL_sift_pulsarx=/data/20251216/LSI/code/ACCEL_sift_drifting.py
+ACCEL_sift_pulsarx=/data/code/ACCEL_sift_drifting.py
 # Only accel:
 python ""${ACCEL_sift_pulsarx}"" -ACCEL 20 -minP 1 -maxP 2800000 -minS 1 2> cands_cmd.txt
 # For jerk search:
-#python ""${ACCEL_sift_pulsarx}"" -ACCEL 300 -JERK 900 -minP 2. -maxP 10.8 -minS 6 2> pulsarX_cands.List
+#python ""${ACCEL_sift_pulsarx}"" -ACCEL 300 -JERK 900 -minP 2. -maxP 10.8 -minS 6 2> cands_cmd.txt
 
 # For folding
 MASK_FILE=$(ls ../*.mask)
